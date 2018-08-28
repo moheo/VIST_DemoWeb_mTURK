3e12c15c53771a30b44a8aa793c686bd4a291a63
SEONIL <seonilson92@gmail.com>
2018-08-28 22:47:06 +0900

    preprocess_prototype.ipynb is verified. Preprocess.py is under review(argparse, pathlib)

--- pre_post_processing/Preprocess.py
+++ pre_post_processing/Preprocess.py
@@ -1,84 +1,142 @@
-# this is class - oriented version of preprocessing_prototype.ipynb
+# this is class - oriented version of 
+#   preprocessing_prototype.ipynb
+#   +mate_storyid_normtext.py
+#   +extract_storyid2photoid.ipynb
 
-'''
-*argv
-def function(*argv):
-    ...
-
-a = (1,2,3) --> function(a)     arg1: 1
-                                arg2: 2
-                                arg3: 3
-
-def function(**kwargs):
-    ...
-
-a = {"key1": 3,"key3": 1, "key2: 2} --> function(a)     "key1": 3
-                                                        "key2": 2
-                                                        "key3": 1
-'''
-#import argparse
 import json
 from pathlib import Path as p
+from pathlib import PurePath as pp
+import argparse 
+import os
 
 '''
 note that # storyid ~5053  and  # photostream ~2019 which implies "2.5 sid / ps"
 
-we need
-test.sis.json path
-sid2ps.json path (make it by your hand)
-                            ^^^^^^^^^^
-htmldir
-resultdir dir for saving resulted jsons
-modelout_path: modelout in example submission format to be parsed according to the html numbers
-(../old_withpy/example_sub_format_in_a_glance.json)
-nunspl: number of splits to be made (need to divide 200!)
+================================================================================
+usage: 
+
+python Preprocess.py [--testsis] [--sid2photoseq] [--result_dir] [--html_dir] [--modelout] [--numsplit]
+
+or you can "from '../pre_post_processing/Preprocess' import *" 
+and simply make use of 
+
+    a = Prep()
+    a.run_prep()
+
+to run with default options
+see codes at the bottom starting with 
+    if __name__ == "__main__":   
+
+
+description:
+
+This code is designed for preprocessing 
+    model_output.json, 
+    htmls named with pids, and 
+    storyID2photoseq.jsons
+to prep
+    ../webstorm_server/data/
+        -200html_sid2photostream.json
+        ../webstorm_server/data/{model_name}_splits/
+            -{model_name}_normtxt_spl_0.json
+            -
+            -
+            -{model_name}_normtxt_spl_k.json
+
+
+
+
+params in detail:
+
+'--testsis' testsis.json directory
+'--sid2photoseq' storyID2photoseq.json directory, could be generated from Prep.storyID2ps() with test.sis.json(VIST) 
+'--result_dir' preprocessed files stored here. presumably "**/webstorm_server/data"
+'--html_dir' dir of html examples named with photoids from Kenneth Huang. need to be unzipped first 
+            (https://github.com/windx0303/VIST-Challenge-NAACL-2018/tree/master/human_eval_interface)")
+'--modelout' your model output with the same format as ../old_withpy/example_sub_format_in_a_glance.json
+'--numsplit' number of splits for making {model_name}_normtxt_spl_k.json. need to divide 200.
+==========================================================================================================================
 
 '''
 
-#class opt:
 
-class Prep:
+class Prep(Opt):
     # calling class method (self.method) inside __init__() is OK.
-    def __init__(self, *args): 
+    def __init__(self, opt): 
         super().__init__()
         
-        #for preparing html extraction of photosequences, storyIDs, normtxts
-        self.testsis_path = testsis_path
-        self.sid2ps_path = sid2ps_path
+        # consider wrapping paths in __init__() with Path constructor and methods
+        # .format need to use p.join
+        # p.name
+
+        # for preparing html extraction of photosequences, storyIDs, normtxts
+        self.testsis_path = pp(opt.testsis_path)
+        self.sid2ps_path = pp(opt.sid2ps_path)
+        self.resultdir = pp(opt.resultdir)
 
         with open(self.testsis_path, "r") as f_ts, \
             open(self.sid2ps_path, "r") as f_s2p,  \
-            open("{}/testset_sid2normtxt.json".format(resultdir), "w") as fw_ts2txt_all, \
-            open("{}/200html_sid2photostream.json".format(resultdir), "w") as fw_ps, \
-            open("{}/200html_sid2norm_txt.json".format(resultdir), "w") as fw_nt:
+            open(self.resultdir / "/testset_sid2normtxt.json", "w") as fw_ts2txt_all, \
+            open(self.resultdir / "/200html_sid2photostream.json", "w") as fw_ps, \
+            open(self.resultdir / "/200html_sid2norm_txt.json", "w") as fw_nt:
+            #open("{}/testset_sid2normtxt.json".format(self.resultdir), "w") as fw_ts2txt_all, \
+            #open("{}/200html_sid2photostream.json".format(self.resultdir), "w") as fw_ps, \
+            #open("{}/200html_sid2norm_txt.json".format(self.resultdir), "w") as fw_nt:
             
-            # work here
+            #work here
             self.testsis = json.load(f_ts)
             self.s2p = json.load(f_s2p)
             self.s2nt = self.storyID2normtxt(self.testsis['annotations'])
             json.dump(self.s2nt, fw_ts2txt_all)
             
-            self.self.htmldir = self.htmldir
-            self.html_storyids_list = self.get_storyid_from_htmls(self)
-            self.h_sid2ps, self.h_sid2nt = self.html_sids2ps_n_nt(self)
+            self.htmldir = pp(opt.htmldir)
+            self.html_storyids_list = self.get_storyid_from_htmls()
+            self.h_sid2ps, self.h_sid2nt = self.html_sids2ps_n_nt()
             json.dump(self.h_sid2ps, fw_ps)
             json.dump(self.h_sid2nt, fw_nt)
 
-        #for spliting and parsing according to prep(200 htmls) above
-        self.modelname = modelout_p.split("/")[-1].split(".json")[0]
-        self.resultdir = resultdir
-        self.spldir = "{rd}/{modelname}_splits/".format(rd=self.resultdir, spldir=self.modelname)
-        self.numspl = numspl
-        if not p.is_dir(self.spldir):
-            p.mkdir(self.spldir, parents = True)
         
-        with open(modelout_path, "r") as f_modelout,\
-            open("{res}/200_{modelname}_normtxt.json".format(res=self.resultdir, modelname=self.modelname), 'w' ) as fw_m_whole:            
+        #for spliting and parsing according to prep(200 htmls) above
+        self.modelout_path = pp(opt.modelout_path)
+        self.modelname = self.modelout_path.stem
+        self.spldir = self.resultdir / "{}_splits/".format(self.modelname)
+        self.numspl = opt.numspl
+        
+        if not p.is_dir(self.spldir):
+            p.mkdir(self.spldir, parents = True) # same as $mkdir -p (if it needs parents, make it!)
+        
+        with open(self.modelout_path, "r") as f_modelout,\
+            open(self.resultdir / '200_{modelname}_normtxt'.format(self.modelname), 'w' ) as fw_m_whole:
+            #open("{res}/200_{modelname}_normtxt.json".format(res=self.resultdir, modelname=self.modelname), 'w' ) as fw_m_whole:            
+            
             self.modelout = json.load(f_modelout)
-            self.model_normtxt = self.modelout_sid2normtxt(self)
+            self.model_normtxt = self.modelout_sid2normtxt()
             json.dump(self.model_normtxt, fw_m_whole)
 
-        
+
+    # if this has problem see extract_storyid2photoid.ipynb
+    # if other part of the script has problem you may suspect
+    #       - argparse, pathlib 
+    #   until now, below two seems correct (verified by naked eyes on codes)
+    #       - mate_storyid_normtext.py
+    #       - preprocessing_prototype.ipynb
+
+    def storyID2ps(self):
+        # init
+        storyid2photoid_dict = {}
+        photo_stream_list = list()
+        current_story_id = self.test_sis['annotations'][0][0]['story_id'] 
+        # go loop
+        for elem in annot_list:
+            photoid = elem[0]['photo_flickr_id']
+            
+            if current_story_id != elem[0]['story_id']:
+                storyid2photoid_dict[current_story_id] = photo_stream_list # dict update 
+                current_story_id = elem[0]['story_id'] # current storyid update 
+                photo_stream_list = list() # init photo_stream_list and append new photoid
+            #else when story_id stays the same
+            photo_stream_list.append(photoid)
+        return storyid2photoid_dict
 
     
     def storyID2normtxt(self):
@@ -147,6 +205,7 @@ class Prep:
     def split_normtxt(self):
         print("split model:{modelname} into {k} splits".format(modelname=self.modelname, k=self.numspl))
         print("to be saved at {res}".format(self.resultdir))
+        
         keys200 = list(self.modelout_normtxt.keys())
         keysplits = [ keys200[20*i:20*(i+1)] for i in range(self.numspl) ]
 
@@ -156,22 +215,50 @@ class Prep:
             model_dicts_list.append(dict(zip(keys,stories))) # dict,zip returns len=20 dict. list of k dicts of 200//k entries (k need to divide 200 w/o residual)
 
         for i in range(self.numspl):
-            with open("{spldir}/{mname}_normtxt_spl_{idx}.json".format(spldir= self.spldir, 
-                                                                        mname=self.modelname, 
-                                                                        idx=i), "w") as fw:
+            with open(self.spldir / "/{mname}_normtxt_spl_{idx}.json".format(mname=self.modelname, idx=i), "w") as fw:
                 json.dump(model_dicts_list[i], fw)
 
 
     def run_prep(self):
         if 200%self.numspl != 0:
             exit("numspl need to divide 200!")
-        for file in [self.testsis_path, self.modelout_path]:
+        
+        for file in [self.testsis_path, self.modelout_path,self.sid2ps_path]:
             if not p.is_file(file):
                 exit("{file} doesnt exist".format(file))
-        
-        for directory in [self.htmldir, self.resultdir]
-            if not p.is_dir(directory):
-                exit("{dir} doesnt exist".format(directory))
+            else:
+                print("found input {file}".format(file))
         
 
-if __name__ == "__main__":
+        if not p.is_dir(self.htmldir): 
+            exit("htmldir isnt exist")
+        elif len(os.listdir(self.htmldir)) == 0: 
+            exit("htmldir is empty")
+        else:
+            print("found {} files in htmldir".format(len(os.listdir(self.htmldir))))
+        
+        if not p.is_dir(self.resultdir):
+            exit("{dir} doesnt exist".format(directory))
+        self.split_normtxt()
+
+
+if __name__=="__main__":
+    option=argparse.ArgumentParser()
+    option.add_argument('--testsis' , help="path of [test.sis.json] file (VIST dataset)", \
+                        dest=testsis_path, default= "/home/seonils/data_storage/VIST_DemoWeb_mTURK/pre_post_processing/test.story-in-sequence.json")
+    option.add_argument('--sid2photoseq' , help="path of [storyID to photostream json] file (you have to make it your own)", \
+                        dest=sid2ps_path, default= "/home/seonils/data_storage/VIST_DemoWeb_mTURK/pre_post_processing/test.storyid2photos_list.json")
+    option.add_argument('--result_dir', help="preprocessed jsons ready for server use is stored here. If not exists, make it", \
+                        dest=resultdir, default= "/home/seonils/data_storage/VIST_DemoWeb_mTURK/webstorm_server/data/" )
+    option.add_argument('--html_dir', help="dir that \"this\" html files placed in your local, need to be unzipped first (https://github.com/windx0303/VIST-Challenge-NAACL-2018/tree/master/human_eval_interface)", \
+                        dest=htmldir, default= "/home/seonils/data_storage/vist200web/www")
+    option.add_argument('--modelout', help="model inference output need to be the same format as ../old_withpy/example_sub_format_in_a_glance.json", \
+                        dest=modelout_path, default= "/home/seonils/data_storage/vist200web/www")
+    option.add_argument('--numsplit', type=int , help="number of splits you want (splits sid:ps dict with 200 entries into k splits)", dest=numspl, default= 10)
+
+    preprocess = Prep(option)
+
+    preprocess.run_prep()
+    #done!
+
+
--- pre_post_processing/mate_storyid_normtext.py
+++ pre_post_processing/mate_storyid_normtext.py
@@ -1,10 +1,6 @@
-#-*- coding: utf-8 -*-
-
 # storyID: normalized_story
 
 import json
-import numpy as np
-import pickle as pkl
 
 def mate(mate_dict,subm_story_list, storyid2photos_dict, storyID): # storyID is 5digit number, str
     photostream_list = storyid2photos_dict[str(storyID)]
@@ -13,7 +9,7 @@ def mate(mate_dict,subm_story_list, storyid2photos_dict, storyID): # storyID is 
             mate_dict[str(storyID)] = storyelem["story_text_normalized"]
     return None 
 
-with open("example_submission_file.test.json", "rb") as ex_sub, open("test.storyid2photos_list.json", "r") as id2stream:
+with open("example_submission_file.test.json", "r") as ex_sub, open("test.storyid2photos_list.json", "r") as id2stream:
     ex_subm_dict = json.load(ex_sub)
     storyid2photos_dict = json.load(id2stream)
     subm_story_list = ex_subm_dict["output_stories"]
@@ -25,9 +21,5 @@ for i in range(45530 , 50584):
     mate(mate_dict, subm_story_list, storyid2photos_dict, i)
 
 print(len(mate_dict))
-with open("test.storyid2txt.pkl", "wb") as f:
-    pkl.dump(mate_dict, f)
-
-with open("test.storyid2txt.pkl", "rb") as fpkl, open("test.storyid2txt.json", "w") as fjson:
-    pkl_data = pkl.load(fpkl)
-    json.dump(pkl_data, fjson, ensure_ascii=True, indent=4)
+with open("./test.storyid2photos_list.json", "w") as f:
+    json.dump(mate_dict, f)
