e4a60e8cd1e63d5e8a363e71f5c560716b74e90f
SEONIL <seonilson92@gmail.com>
2018-09-04 00:28:50 +0900

    33023 crcn d596

--- webstorm_server/app.js
+++ webstorm_server/app.js
@@ -77,7 +77,7 @@ db.once('open', () => {
   console.log('Connected to mongod server');
 });
 
-mongoose.connect('mongodb://localhost/mmtest');
+mongoose.connect('mongodb://localhost/crcn5ep');
 
 /*
 e.g.mongodb://localhost/glacnet_fa24
